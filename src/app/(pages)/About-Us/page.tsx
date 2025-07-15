60b76e3a1d5a8df86ad0dbf8f53b5803e3dfb138
ShikharThaQuri <shikharchand4321@gmail.com>
2025-07-15 19:57:11 +0545

    About us page design and static data banayera sakiyo

--- src/app/(pages)/About-Us/page.tsx
+++ src/app/(pages)/About-Us/page.tsx
@@ -49,31 +49,91 @@ export default function AboutUsPage() {
         />
       </section>
 
-      <section>
+      <section className="py-[2rem]">
         <h1 className="text-center text-[#fb5607] text-[2rem] font-bold">
           Our Staffs
         </h1>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-transparant">
-          <div>
+        <div className="mt-[8rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-transparant">
+          <div className="bg-[#dda15e] h-auto text-center rounded-lg shadow-xl/30">
             <Image
               src={Image3}
               alt="Staff"
               width={800}
               height={600}
-              className="w-full h-auto object-cover object-center mt-10"
+              className="w-[10rem] h-[10rem] mt-[-5rem] rounded-[5rem] object-cover object-center mx-auto"
             />
+
+            <div className="py-4 px-2">
+              <h1 className="text-[1.4rem] font-bold">Sabita Rawal</h1>
+              <p className="text-gray-800">Accountain</p>
+              <p className="mt-[1rem] text-[1.1rem]">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
           </div>
 
-          <div>
+          <div className="bg-[#dda15e] h-auto text-center rounded-lg shadow-xl/30">
             <Image
               src={Image3}
               alt="Staff"
               width={800}
               height={600}
-              className="w-full h-auto object-cover object-center mt-10"
+              className="w-[10rem] h-[10rem] mt-[-5rem] rounded-[5rem] object-cover object-center mx-auto"
             />
+
+            <div className="py-4 px-2">
+              <h1 className="text-[1.4rem] font-bold">Sabita Rawal</h1>
+              <p className="text-gray-800">Accountain</p>
+              <p className="mt-[1rem] text-[1.1rem]">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
           </div>
+          <div className="bg-[#dda15e] h-auto text-center rounded-lg shadow-xl/30">
+            <Image
+              src={Image3}
+              alt="Staff"
+              width={800}
+              height={600}
+              className="w-[10rem] h-[10rem] mt-[-5rem] rounded-[5rem] object-cover object-center mx-auto"
+            />
+
+            <div className="py-4 px-2">
+              <h1 className="text-[1.4rem] font-bold">Sabita Rawal</h1>
+              <p className="text-gray-800">Accountain</p>
+              <p className="mt-[1rem] text-[1.1rem]">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div className="bg-[#dda15e] h-auto text-center rounded-lg shadow-xl/30">
+            <Image
+              src={Image3}
+              alt="Staff"
+              width={800}
+              height={600}
+              className="w-[10rem] h-[10rem] mt-[-5rem] rounded-[5rem] object-cover object-center mx-auto"
+            />
+
+            <div className="py-4 px-2">
+              <h1 className="text-[1.4rem] font-bold">Sabita Rawal</h1>
+              <p className="text-gray-800">Accountain</p>
+              <p className="mt-[1rem] text-[1.1rem]">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="text-center mt-[2rem]">
+          <button className="px-[1rem] py-[0.8rem] bg-[#48cae4]">
+            Show More
+          </button>
         </div>
       </section>
     </main>
