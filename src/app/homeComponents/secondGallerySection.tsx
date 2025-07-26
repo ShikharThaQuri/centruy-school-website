42a68f234f83e245b8b5f862206ac7a3d2497b27
ShikharThaQuri <shikharchand4321@gmail.com>
2025-07-26 21:41:30 +0545

    Home Page Gallery Section All done

--- src/app/homeComponents/secondGallerySection.tsx
+++ src/app/homeComponents/secondGallerySection.tsx
@@ -1,72 +1,12 @@
-import Image from "next/image";
-import photo1 from "@/images/profile-6.jpg";
-import photo2 from "@/images/profile-7.jpg";
-import photo3 from "@/images/profile-9.jpg";
-
-const imageDivStyles = "bg-gray-200 h-[15rem] rounded-lg shadow-md";
+import { Suspense } from "react";
+import SwiperGallery from "./swiperGallerySection";
 
 export default function GallerySection() {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 bg-transparant">
-      <div className={imageDivStyles}>
-        <Image
-          src={photo1}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo2}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo3}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo1}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-
-      <div className={imageDivStyles}>
-        <Image
-          src={photo2}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo1}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo2}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-      <div className={imageDivStyles}>
-        <Image
-          src={photo3}
-          alt="Gallery Item 1"
-          className="w-full h-full rounded-lg mb-4 object-cover object-center"
-        />
-      </div>
-
-      {/* Add more items as needed */}
+    <section>
+      <Suspense fallback={<h1>Loading....</h1>}>
+        <SwiperGallery />
+      </Suspense>
     </section>
   );
 }
---
+++ src/app/homeComponents/swiperGallerySection.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { eventType } from "@/models/Gallery";
+import Image from "next/image";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { getAllEvents } from "@/services/admin/gallery";
+
+const imageDivStyles = "bg-gray-200 h-[15rem] rounded-lg shadow-md";
+
+export default function SwiperGallery() {
+  const [data, setData] = useState<eventType[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getAllEvents();
+        if (!result) {
+          console.log("wrong");
+        }
+
+        setData(result?.data || []);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  return (
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={3}
+      navigation={true}
+      // pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      loop={true}
+      modules={[Pagination, Navigation, Autoplay]}
+    >
+      {data.map((items: eventType, i: number) => (
+        <SwiperSlide key={i} className={imageDivStyles}>
+          <Image
+            src={
+              items.Images[Math.floor(Math.random() * items.Images.length)]
+                ?.image_url
+            }
+            width={300}
+            height={300}
+            alt="Gallery Item 1"
+            className="w-full h-[16rem] rounded-lg object-cover object-center"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
