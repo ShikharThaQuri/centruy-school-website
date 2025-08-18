fd7a4b53570e8b80d39e9842712406767aef9796
ShikharThaQuri <shikharchand4321@gmail.com>
2025-08-18 14:52:13 +0545

    event page mobile view image scroll done

--- src/components/eventSectionShowImage.tsx
+++ src/components/eventSectionShowImage.tsx
@@ -17,31 +17,48 @@ export default function EventSectionShowImage({
     <section
       className={`${
         popUp ? "" : "hidden"
-      } fixed top-0 bottom-0 left-0 right-0 bg-black flex justify-center items-center text-white z-110 relatives`}
+      } fixed top-0 bottom-0 left-0 right-0 bg-black  text-white z-110 relatives`}
     >
-      <Image
-        src={imageUrl || items.Images[0].image_url}
-        alt="Image"
-        width={1000}
-        height={500}
-        className="w-full h-[auto] object-cover object-center"
-      />
-
-      <div
-        className="absolute left-[1rem] right-[1rem] bottom-[1rem] z-20 
-      flex items-center border p-[0.5rem] backdrop-blur-sm bg-white/30 overflow-x-scroll"
-      >
+      {/* mobile view design!!! */}
+      <div className="md:hidden flex items-center snap-x snap-mandatory overflow-x-scroll w-full h-full">
         {items.Images.map((img, i) => (
           <Image
             key={i}
             src={img.image_url}
-            onClick={() => setImageUrl(img.image_url)}
             alt="Image"
-            width={400}
+            width={500}
             height={200}
-            className="w-[10rem] md:w-[15rem] lg:[20rem] h-[5rem] lg:h-[8rem] object-cover object-center"
+            className={`w-full h-auto snap-start`}
           />
         ))}
+      </div>
+
+      {/* other then mobile view design View!! */}
+      <div className="hidden md:flex justify-center items-center">
+        <Image
+          src={imageUrl || items.Images[0].image_url}
+          alt="Image"
+          width={1000}
+          height={500}
+          className="w-full h-[auto] object-cover object-center"
+        />
+
+        <div
+          className="absolute left-[1rem] right-[1rem] bottom-[1rem] z-20 
+      flex items-center border p-[0.5rem] backdrop-blur-sm bg-white/30 overflow-x-scroll"
+        >
+          {items.Images.map((img, i) => (
+            <Image
+              key={i}
+              src={img.image_url}
+              onClick={() => setImageUrl(img.image_url)}
+              alt="Image"
+              width={400}
+              height={200}
+              className={`w-[10rem] md:w-[15rem] lg:[20rem] h-[5rem] lg:h-[8rem] object-cover object-center`}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
